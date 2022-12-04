724e6e2f7b482a5850b76ce1dd951dd80904db9a
Kevin Wong <kevin@kevinwong.co>
2022-12-03 21:57:15 -0500

    Fixed NextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    client_id: process.env.client_id,
+    client_secret: process.env.client_secret,
+    refresh_token: process.env.refresh_token,
+  }
 }
 
 module.exports = nextConfig
